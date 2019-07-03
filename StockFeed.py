df1e30fdb3a1a8459d652dcc8b73c93de50541e7
Maykon Meneghel <maykonmeneghel@icloud.com>
2019-07-03 11:42:31 -0300

    Alterações para: Puxar a série histórica de uma ação Enviar dados como: abertura, fechamento, alta, baixa e volume para o rdis Testar algumas regras com os dados que foram recebidos no motor de inferência

--- StockFeed.py
+++ StockFeed.py
@@ -1,22 +1,51 @@
+# Libraries
 import time
-from iexfinance.stocks import Stock
-# from iexfinance.stocks import get_historical_data
+from iexfinance.stocks import get_historical_intraday
 import os
 import requests
+from datetime import datetime
 
 # SET API PERMISSIONS
 os.environ["IEX_API_VERSION"] = "iexcloud-sandbox"
 os.environ["IEX_TOKEN"] = "Tpk_d9cc24d84d83489d88e9faeaf93dbaf8"
 
-aapl = Stock("AAPL")
+# Set data interval
+date = datetime(2019, 5, 23)
 
-URL = "http://localhost:5000/risk/facts"
+# Get historical data minute by minute
+data = get_historical_intraday('AAPL', date, output_format='pandas')
 
+# Data
+openData = data[['open']]
+closeData = data[['close']]
+highData = data[['high']]
+lowData = data[['low']]
+volumeData = data[['volume']]
+
+# Request settings
+URL = "http://localhost:5000/stock/facts"
 headers = {'content-type': 'application/json'}
 
-while True:
-    fechamento = aapl.get_price()
-    volume = aapl.get_volume()
-    data = '{"values": [' + str(fechamento) + ',' + str(volume) + ']}'
+# Send a request to each according
+index = 0
+while index < len(closeData):
+    # Separate the data
+    openValue = openData.values[index]
+    closeValue = closeData.values[index]
+    highValue = highData.values[index]
+    lowValue = lowData.values[index]
+
+    # Print the data as it will be sent
+    print(str(openValue) + str(closeValue) + str(highValue) + str(lowValue))
+
+    # Encoding JSON data
+    data = r'{"close":' + str(closeValue) + ', "open":' + str(openValue) + ', "high":' + str(highValue) + ', "low":' + str(lowValue) + '}'
+
+    # Send request
     response = requests.post(URL, headers=headers, data=data)
+
+    # Wait 1 second
     time.sleep(1)
+
+    # Increment index control
+    index += 1
--- StockInference.py
+++ StockInference.py
@@ -17,38 +17,42 @@ pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
 r = redis.Redis(connection_pool=pool)
 r.flushall()
 
-with ruleset('risk'):
-    # matching primitive array
+with ruleset('stock'):
 
-    @when_all(m.values.allItems((item > 100) | (item < 150)))
-    def rule1(c):
-        print('fraud 1 detected {0}'.format(c.m.values))
+    @when_all(m.close.allItems(item > 185) & m.open.allItems(item > 150) & m.high.allItems(item > 185))
+    def rule0(c):
+        print("entrou")
 
-
-    # matching object array
-    @when_all(m.values.allItems((item.amount < 250) | (item.amount >= 300)))
-    def rule2(c):
-        print('fraud 2 detected {0}'.format(c.m.values))
-
-
-    # pattern matching string array
-    @when_all(m.cards.anyItem(item.matches('three.*')))
-    def rule3(c):
-        print('fraud 3 detected {0}'.format(c.m.cards))
-
-
-    # matching nested arrays
-    @when_all(m.values.anyItem(item.allItems(item < 100)))
-    def rule4(c):
-        print('fraud 4 detected {0}'.format(c.m.values))
+    # # matching primitive array
+    # @when_all(m.values.anyItem(item > 0))
+    # def rule1(c):
+    #     print('fraud 1 detected {0}'.format(c.m.values))
+    #
+    #
+    # # matching object array
+    # @when_all(m.values.allItems((item.amount < 250) | (item.amount >= 300)))
+    # def rule2(c):
+    #     print('fraud 2 detected {0}'.format(c.m.values))
+    #
+    #
+    # # pattern matching string array
+    # @when_all(m.cards.anyItem(item.matches('three.*')))
+    # def rule3(c):
+    #     print('fraud 3 detected {0}'.format(c.m.cards))
+    #
+    #
+    # # matching nested arrays
+    # @when_all(m.values.anyItem(item.allItems(item < 100)))
+    # def rule4(c):
+    #     print('fraud 4 detected {0}'.format(c.m.values))
 
 
     @when_start
     def start(host):
-        host.post('risk', {'values': [150, 300]})
-        host.post('risk', {'values': [{'amount': 200}, {'amount': 300}, {'amount': 450}]})
-        host.post('risk', {'cards': ['one card', 'two cards', 'three cards']})
-        host.post('risk', {'values': [[10, 20, 30], [30, 40, 50], [10, 20]]})
-
+        host.post('stock', {'value': [150], 'open': [150], 'high': [150], 'low': [150], 'volume': [150]})
+        # host.post('stock', {'open': [100]})
+        # host.post('stock', {'high': [100]})
+        # host.post('stock', {'low': [100]})
+        # host.post('stock', {'volume': [100]})
 
 run_all([{'host': 'localhost', 'port': 6379}])
